65fa8179cbbfe7301d06812d7aecd8304dfb2d2d
Nellis,Dean <dean.nellis@lexmark.com>
2018-10-24 16:38:03 -0400

    Added in check to see if user coords match coords passed in by props

--- src/components/MapViewPage.js
+++ src/components/MapViewPage.js
@@ -1,78 +1,28 @@
-// @flow
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MapView from './MapView';
 
-import React, { createRef, Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+export default class MapViewPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-type State = {
-  hasLocation: boolean,
-  start_coords: {
-        lat: number,
-        lng: number,
-        zoom: number,
-    },
-    current_coords: {
-        lat: number,
-        lng: number,
-    }
-};
-
-export default class MapViewPage extends Component<{}, State> {
-  state = {
-    hasLocation: false,
-    zoom: 15,
-    start_coords: {
-        lat: 38.045,
-        lng: -84.5,
-    },
-    current_coords: {
-        lat: null,
-        lng: null,
-    },
-  };
-
-  mapRef = createRef();
-
-  handleClick = () => {
-    const map = this.mapRef.current
-    if (map != null) {
-        map.leafletElement.locate()
+    this.state = {
+      checkpoint: {
+        lat: 38.068224,
+        lon: -84.4890112
+      }
     }
   }
-
-  handleLocationFound = (e: Object) => {
-    this.setState({
-        hasLocation: true,
-        current_coords: e.latlng,
-    })
-  }
-
   render() {
-    const position = [this.state.start_coords.lat, this.state.start_coords.lng]
-    const current_marker = this.state.hasLocation ? (
-        <Marker position={this.state.current_coords}>
-            <Popup>You are here</Popup>
-        </Marker>
-    ) : null
-
     return (
-      <Map
-      center={this.state.start_coords}
-      zoom={this.state.zoom}
-      ref = {this.mapRef}
-      onClick={this.handleClick}
-      onLocationfound={this.handleLocationFound}
-      >
-        <TileLayer
-          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={this.state.start_coords}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        {current_marker}
-      </Map>
+      <div>
+          <h3>Map View Page</h3>
+          <Link to="/">Back to Events</Link>
+          <p>
+            Map View | <Link to="/list">List view</Link>
+          </p>
+          <MapView checkpoint={this.state.checkpoint}/>
+      </div>
     );
-  };
+  }
 }
